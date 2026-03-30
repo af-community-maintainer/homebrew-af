db7124d90ab62052a812ae768220594f1bdd332d
af-community-maintainer <af-community-maintainer@proton.me>
2026-03-30 13:10:28 -0700

    update archform to 2.6.4.4

---
+++ Casks/archform.rb
@@ -0,0 +1,11 @@
+cask "archform" do
+  version "2.6.4.4"
+  sha256 "c8c2a38d97f521473ae624d8b0a34cc225fe9c6c917028f9eaf0384a4c13fbe1"
+
+  url "https://download.archform.com/ArchForm_mac_2_6_4_4.zip"
+  name "ArchForm Standard"
+  desc "Professional Orthodontic Design"
+  homepage "https://archform.com"
+
+  app "ArchForm.app"
+end
