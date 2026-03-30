746ce82726ba1765096f72ad8bbfb3d5528ffb52
af-community-maintainer <af-community-maintainer@proton.me>
2026-03-30 13:10:45 -0700

    update archform-cnc to 2.6.4.13

---
+++ Casks/archform-cnc.rb
@@ -0,0 +1,11 @@
+cask "archform-cnc" do
+  version "2.6.4.13"
+  sha256 "44c22a20902fda04fa5974b4eab934f97d5e7c5c8a3071407f8744f7cfecb67a"
+
+  url "https://download.archform.com/ArchForm_mac_cnc_2_6_4_13.zip"
+  name "ArchForm Cnc"
+  desc "Professional Orthodontic Design with CNC tool"
+  homepage "https://archform.com"
+
+  app "ArchForm.app"
+end
